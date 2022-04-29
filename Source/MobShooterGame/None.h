360e66c5d7438809830a6f0bef7858164efd7016
YouCanBuildGames <62948900+YouCanBuildGames@users.noreply.github.com>
2022-04-29 11:46:58 +0200

    Changed peoject to C++ instead of blueprints, added FMOD

---
+++ Source/MobShooterGame/None.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class MOBSHOOTERGAME_API None
+{
+public:
+	None();
+	~None();
+};
